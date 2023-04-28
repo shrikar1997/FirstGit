ddf7237012cb9616bb0ba3d8f0e41543aec64714
shrikar1997 <shrikar1997@gmail.com>
2023-04-28 21:42:48 +0530

    Get Elements by Class

--- dom.js
+++ dom.js
@@ -12,15 +12,25 @@ console.log(document.forms);
 console.log(document.links);
 console.log(document.images);*/
 //GETELEMENTBYID//
-let headerTitle = (document.getElementById('header-title'));
-let header = document.getElementById('main-header');
-console.log(headerTitle);
+//let headerTitle = (document.getElementById('header-title'));
+//let header = document.getElementById('main-header');
+//console.log(headerTitle);
 //headerTitle.textContent = 'Hello';
-console.log(headerTitle.textContent);
+//console.log(headerTitle.textContent);
 //headerTitle.innerHTML = '<h3>Hello</h3>';
-header.style.borderBottom='solid 3px #000';
+//header.style.borderBottom='solid 3px #000';
 
-let title =(document.getElementById('main'));
-title.style.color = 'green';
-title.style.fontStyle = 'bold';
+//let title =(document.getElementById('main'));
+//title.style.color = 'green';
+//title.style.fontStyle = 'bold';
+
+//GET ITEMS BY CLASS//
+
+let item = document.getElementsByClassName('list-group-item');
+item[1].style.fontWeight = 'bold';
+item[2].style.backgroundColor = 'green';
+
+for(let i=0; i<=item.length; i++){
+    item[i].style.fontWeight = 'bold';
+}
 
