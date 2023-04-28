91c1b2e796f868751c43896bfa2265de8455feed
shrikar1997 <shrikar1997@gmail.com>
2023-04-28 21:20:31 +0530

    Newcode

---
+++ dom.js
@@ -0,0 +1,26 @@
+/*console.log(document.location);
+console.log(document.URL);
+console.log(document.title);
+//document.title = 123;
+console.log(document.doctype);
+console.log(document.head);
+console.log(document.body);
+console.log(document.getElementsByTagName);
+console.log(document.getElementsByClassName('body'));
+//document.querySelectorAll[10].textContent = 'Hello';
+console.log(document.forms);
+console.log(document.links);
+console.log(document.images);*/
+//GETELEMENTBYID//
+let headerTitle = (document.getElementById('header-title'));
+let header = document.getElementById('main-header');
+console.log(headerTitle);
+//headerTitle.textContent = 'Hello';
+console.log(headerTitle.textContent);
+//headerTitle.innerHTML = '<h3>Hello</h3>';
+header.style.borderBottom='solid 3px #000';
+
+let title =(document.getElementById('main'));
+title.style.color = 'green';
+title.style.fontStyle = 'bold';
+
