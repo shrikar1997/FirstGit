1d9bb159f74aec03753b86db38b607acad386f79
shrikar1997 <shrikar1997@gmail.com>
2023-04-30 20:09:24 +0530

    added delete button

--- demo.js
+++ demo.js
@@ -6,35 +6,40 @@ var filter = document.getElementById('filter');
 form.addEventListener('submit', addItem);
 // Delete event
 itemList.addEventListener('click', removeItem);
+// Filter event
+filter.addEventListener('keyup', filterItems);
 
 // Add item
 function addItem(e){
   e.preventDefault();
 
-  //console.log(1);
-
   // Get input value
   var newItem = document.getElementById('item').value;
-
   // Create new li element
   var li = document.createElement('li');
   // Add class
   li.className = 'list-group-item';
   // Add text node with input value
   li.appendChild(document.createTextNode(newItem));
-
   // Create del button element
   var deleteBtn = document.createElement('button');
-
   // Add classes to del button
   deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
-
   // Append text node
   deleteBtn.appendChild(document.createTextNode('X'));
-
   // Append button to li
   li.appendChild(deleteBtn);
 
+  var description = document.getElementsById('description').value;
+  li.appendChild(document.createTextNode(description));
+
+  var editBtn = document.createElement('button');
+  // Add classes to edit button
+  editBtn.className = 'btn-sm float-right submit';
+  // Append text node
+  editBtn.appendChild(document.createTextNode('Edit'));
+  // Append button to li
+  li.appendChild(editBtn);
   // Append li to list
   itemList.appendChild(li);
 }
@@ -49,4 +54,19 @@ function removeItem(e){
   }
 }
 
-
+// Filter Items
+function filterItems(e){
+  // convert text to lowercase
+  var text = e.target.value.toLowerCase();
+  // Get lis
+  var items = itemList.getElementsByTagName('li');
+  // Convert to an array
+  Array.from(items).forEach(function(item){
+    var itemName = item.firstChild.textContent;
+    if(itemName.toLowerCase().indexOf(text) != -1){
+      item.style.display = 'block';
+    } else {
+      item.style.display = 'none';
+    }
+  });
+}
