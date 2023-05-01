6ce227ad607c3fb22beea7d33b01386e4cc16586
shrikar1997 <shrikar1997@gmail.com>
2023-05-01 16:34:48 +0530

    adding multiple users

--- form.js
+++ form.js
@@ -4,11 +4,19 @@ function saveToLocalStorage(event){
     const name = event.target.username.value;
     const email = event.target.email.value;
 
-    localStorage.setItem('name', name);
-    localStorage.setItem('email', email);
+   // localStorage.setItem('name', name);
+    // localStorage.setItem('email', email);
 
     const obj = {name, email};
-    localStorage.setItem('userDetails', JSON.stringify(obj));
+    localStorage.setItem(obj.email, JSON.stringify(obj));
+    showUserOnScreen(obj);
+}
 
+function showUserOnScreen(obj){
+    const parentElement = document.getElementsByClassName('ex');
+    //const childElement = document.createElement('li');
+    //childElement.textContent = obj.name + '-' + obj.email;
+    //parentElement.appendChild(childElement)
 
+    parentElement.innerHTML = parentElement.innerHTML + `${obj.name} ${obj.email}`;
 }
